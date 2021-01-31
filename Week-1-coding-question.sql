ca64caf456fe3011d96fc6cfe269101d51d6f26f
Miao Hu <77250655+MiaoHu17@users.noreply.github.com>
2021-01-31 18:24:05 -0500

    Update Week-1-coding-question.sql

--- Week-1-coding-question.sql
+++ Week-1-coding-question.sql
@@ -1,9 +1,28 @@
+/*
 Question 1
 For all of the questions in this quiz, we are using the Chinook database. All of the interactive code blocks have been setup to retrieve data only from this database.
 
-Retrieve all the records from the Employees table.
+https://archive.codeplex.com/?p=chinookdatabase
+
+Retrieve all the records from the Employees table.*/
 
 select
 *
-from
-employees
+from employees
+
+/*
+Question 2
+Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.
+*/
+
+select 
+firstname, lastname, birthdate, address, city, state 
+from employees
+
+/*
+Question 2
+Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.
+*/
+select * 
+from tracks
+limit 20;
